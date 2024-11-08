e057f16affc81a7c5885dfb504eb4323f1d4c301
Usama-Ashraf27 <usama@karigar.pk>
2024-11-08 03:42:28 -0800

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -74,9 +74,9 @@ function App() {
         <Route
           path="/admin"
           element={
-            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            // <CheckAuth isAuthenticated={isAuthenticated} user={user}>
               <AdminLayout />
-            </CheckAuth>
+            // </CheckAuth>
           }
         >
           <Route path="dashboard" element={<AdminDashboard />} />

bb72f64d22e8220941c58e6fdcc7bf4ffbaf2429
Usama-Ashraf27 <usama@karigar.pk>
2024-11-13 22:36:04 -0800

    fix

--- src/components/shopping-view/address-card.jsx
+++ src/components/shopping-view/address-card.jsx
@@ -16,29 +16,29 @@ function AddressCard({
           ? () => setCurrentSelectedAddress(addressInfo)
           : null
       }
-      className={`cursor-pointer border-2 transition-all ease-in-out duration-300 ${
+      className={`cursor-pointer w-full border-2 transition-all ease-in-out duration-300 ${
         selectedId?._id === addressInfo?._id
           ? "border-red-500 shadow-lg transform scale-105"
           : "border-gray-300 shadow-md"
       } rounded-lg bg-white overflow-hidden`}
     >
-      <CardContent className="grid p-6 gap-4">
+      <CardContent className="p-4 sm:p-6 gap-3 grid">
         <Label className="text-lg font-semibold text-gray-700">Address</Label>
         <p className="text-sm text-gray-600">{addressInfo?.address}</p>
-        
+
         <Label className="text-lg font-semibold text-gray-700">City</Label>
         <p className="text-sm text-gray-600">{addressInfo?.city}</p>
-        
+
         <Label className="text-lg font-semibold text-gray-700">Email</Label>
         <p className="text-sm text-gray-600">{addressInfo?.pincode}</p> {/* Showing pincode as email */}
-        
+
         <Label className="text-lg font-semibold text-gray-700">Phone</Label>
         <p className="text-sm text-gray-600">{addressInfo?.phone}</p>
 
         <Label className="text-lg font-semibold text-gray-700">Notes</Label>
         <p className="text-sm text-gray-600">{addressInfo?.notes}</p>
       </CardContent>
-      <CardFooter className="p-4 flex justify-between bg-gray-50 border-t">
+      <CardFooter className="p-3 flex justify-between bg-gray-50 border-t">
         <Button
           onClick={() => handleEditAddress(addressInfo)}
           className="text-white bg-blue-500 hover:bg-blue-600 transition duration-200 px-4 py-2 rounded-md"

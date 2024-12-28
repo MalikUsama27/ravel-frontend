1137e3920fd8596add95f3cec4e70f85b50ce578
Malik Usama Ashraf <122934021+MalikUsama27@users.noreply.github.com>
2024-12-28 06:40:36 +0500

    Update ViewTwo.jsx

--- src/pages/shopping-view/ViewTwo.jsx
+++ src/pages/shopping-view/ViewTwo.jsx
@@ -38,34 +38,28 @@ const ViewTwo = () => {
         </div>
 
         {/* Image Side - shows below the text on mobile */}
-        {/* <div
+        <div
           className="flex-1 w-full md:w-7/12 bg-cover bg-center h-64 md:h-auto order-2"
           style={{ backgroundImage: `url(${img1})` }} 
         >
-        </div> */}
+        </div>
       </div>
       */}
 
       {/* Second Section */}
       <div className="flex flex-col md:flex-row w-full bg-black mx-auto shadow-md overflow-hidden p-9">
-        {/* Image Side - shows below the text on mobile */}
-        {/* <div
-          className="flex-1 w-full md:w-7/12 bg-cover bg-center h-64 md:h-auto order-2"
-          style={{ backgroundImage: `url(${img})` }} 
-        >
-        
-        </div> */}
-
-        {/* Text Side - shows first on mobile */}
+        {/* Text Side */}
         <div className="flex-1 w-full md:w-5/12 p-6 bg-black text-white flex flex-col justify-center order-1 border border-black md:border-0">
           <h1 className="text-4xl md:text-5xl font-bold text-white mb-4 tracking-wide">WEAR</h1>
           <h2 className="text-2xl md:text-3xl font-semibold">
             <span className="text-red-600">INTERNATIONAL </span>
           </h2>
           <p className="mt-3 text-white text-lg leading-relaxed">
-We’re always happy to hear from our customers. Whether you’re interested in our products or have any inquiries, we’re here to help you.          </p>
+            We’re always happy to hear from our customers. Whether you’re interested in our products or have any inquiries, we’re here to help you.
+          </p>
           <p className="mt-3 text-white text-lg leading-relaxed">
-Lets contact us and our team will be happy to assist you!          </p>
+            Let's contact us, and our team will be happy to assist you!
+          </p>
           <div className="mt-5 flex flex-col md:flex-row justify-center space-x-0 md:space-x-4 space-y-2 md:space-y-0">
             <a 
               href="/shop/contact-us" 
